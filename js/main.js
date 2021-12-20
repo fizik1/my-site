1217cd2370401e9ef87d618626621bebd3b6fe08
Your Name <shahobnarpay@gmail.com>
2021-12-20 11:48:26 +0500

    birinchi

---
+++ js/main.js
@@ -0,0 +1,59 @@
+function icha() {
+    let icha =document.getElementById("icha");
+    let nine=document.getElementById("nine");
+    nine.classList.toggle("nine1");
+    icha.classList.toggle("bxs-chevron-right-square");
+}
+
+
+ function menu() {
+     let nav= document.getElementById('navbar');
+     let manu = document.getElementById("manubtn");
+     let manui= document.getElementById("manui");
+     let head=document.getElementById('heading');
+     head.classList.toggle('heading21')
+     manui.classList.toggle('manui');
+     nav.classList.toggle('navbar21');
+     manu.classList.toggle('manubtn2');
+
+ }
+ function div8() {
+     let rang = document.querySelector('--yellow');
+     document.documentElement.style.setProperty('--yellow', '#fafafa');
+
+ }
+ function div7() {
+    let rang = document.querySelector('--yellow');
+    document.documentElement.style.setProperty('--yellow', '#00FFFF');
+
+}
+function div6() {
+    let rang = document.querySelector('--yellow');
+    document.documentElement.style.setProperty('--yellow', '#00FE9C');
+
+}
+function div5() {
+    let rang = document.querySelector('--yellow');
+    document.documentElement.style.setProperty('--yellow', '#ADFF2F');
+
+}
+function div4() {
+    let rang = document.querySelector('--yellow');
+    document.documentElement.style.setProperty('--yellow', '#D6ED17');
+
+}
+function div3() {
+    let rang = document.querySelector('--yellow');
+    document.documentElement.style.setProperty('--yellow', '#FDBB2E');
+
+}
+function div2() {
+    let rang = document.querySelector('--yellow');
+    document.documentElement.style.setProperty('--yellow', '#EEE8AA');
+
+}
+function div1() {
+    let rang = document.querySelector('--yellow');
+    document.documentElement.style.setProperty('--yellow', '#F5DF4E');
+
+}
